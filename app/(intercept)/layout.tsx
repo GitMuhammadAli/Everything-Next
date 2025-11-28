2087f7ec63f6b4c03a290c2095dc12e9b54ab4aa
malingxoft <malingxoft@gmail.com>
2025-11-29 03:12:28 +0500

    interception

---
+++ app/(intercept)/layout.tsx
@@ -0,0 +1,3 @@
+export default function InterceptLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ app/(intercept)/news/@archive/default.tsx
@@ -0,0 +1,3 @@
+export default function InterceptArchiveDefault() {
+  return <div>Archive slot (intercept variant) – select a news item.</div>;
+}
